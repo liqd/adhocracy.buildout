e132ff137a01fec6a04e31055c59f17d4b81d7d2
Philipp Hagemeister <phihag@phihag.de>
2012-05-03 15:40:03 +0200

    chroot: Require user to be root

--- test_debian_chroot.sh
+++ test_debian_chroot.sh
@@ -10,16 +10,12 @@ if [ "$#" -ne 1 ]; then
 fi
 chroot_path=$1
 
-SUDO_CMD=sudo
-if [ "$(id -u)" -eq 0 ]; then
-	SUDO_CMD=
-fi
-if ! $SUDO_CMD true ; then
-	echo 'sudo failed. Is it installed and configured?'
-	exit 20
+if [ "$(id -u)" -ne 0 ]; then
+	echo 'You need to be root to run this script.'
+	exit 32
 fi
 
-$SUDO_CMD apt-get install -yqq debootstrap
+apt-get install -yqq debootstrap
 
 if [ '!' -d $chroot_path/proc ]; then
 	# Use HHU mirror if available
@@ -27,17 +23,18 @@ if [ '!' -d $chroot_path/proc ]; then
 	if wget -q http://mirror.cs.uni-duesseldorf.de/debian/ -O /dev/null; then
 		MIRROR=http://mirror.cs.uni-duesseldorf.de/debian
 	fi
-	$SUDO_CMD debootstrap squeeze $chroot_path $MIRROR
+	debootstrap squeeze $chroot_path $MIRROR
 fi
 
-echo adhocracy-chroot | $SUDO_CMD tee $chroot_path/etc/debian_chroot >/dev/null
+echo adhocracy-chroot > $chroot_path/etc/debian_chroot
 
 # sudo complains if it can't resolve the hostname, suppress that
 if ! grep -P -q "\s$(hostname)(?:\s|$)" $chroot_path/etc/hosts; then
-	sudo sh -c "echo 127.0.0.1 $(hostname) >> $chroot_path/etc/hosts"
+	echo 127.0.0.1 $(hostname) >> $chroot_path/etc/hosts
 fi
 
-$SUDO_CMD tee $chroot_path/adhocracy-runtests.sh >/dev/null <<EOF
+
+cat >$chroot_path/adhocracy-runtests.sh <<EOF
 #!/bin/sh
 # This is the test script, run by the user adhocracy
 
@@ -47,22 +44,34 @@ cd /home/adhocracy/adhocracy_buildout
 . bin/activate
 
 bin/supervisord
-sleep 10 # TODO find a better way to wait for service startup
+python ./adhocracy.buildout/etc/test-port-free.py -o -g 10 5010 # Wait for supervisord to start
 
 # Fail if not all services are marked as running
-bin/supervisorctl status | grep -vq RUNNING
+if bin/supervisorctl status | grep -vq RUNNING; then
+	echo "Failed to start all services."
+	bin/supervisorctl status
+	exit 31
+fi
+
+./paster_interactive.sh &
+paster_pid=$!
+
+# TODO DEBUG
+echo RUNNING AS PID ${paster_pid}
+netstat -ltpn
 
 sleep 10 # TODO find a better way to wait for service startup
 
+
 # TODO run actual tests
-wget -O /dev/null http://adhocracy.lan:5001/
+wget -nv -O /dev/null http://adhocracy.lan:5001/
 
-
+kill "$paster_pid"
 bin/supervisorctl shutdown
 EOF
-sudo chmod a+x $chroot_path/adhocracy-runtests.sh
+chmod a+x $chroot_path/adhocracy-runtests.sh
 
-$SUDO_CMD tee $chroot_path/test_in_chroot.sh >/dev/null <<EOF
+cat >$chroot_path/test_in_chroot.sh <<EOF
 #!/bin/sh
 # Setup this chroot and execute the tests
 
@@ -96,16 +105,19 @@ if [ ! -d /usr/lib/locale ]; then
 fi
 apt-get install -yqq make sudo ca-certificates
 
-
 cd /home/adhocracy
 su adhocracy -c 'wget -nv https://bitbucket.org/phihag/adhocracy.buildout/raw/default/build_debian.sh -O build_debian.sh && sh build_debian.sh -A'
 
 rm -f /etc/sudoers
 
-su adhocracy -c '/adhocracy-runtests.sh'
+if su adhocracy -c '/adhocracy-runtests.sh'; then
+	echo TESTS PASSED, leaving chroot ...
+else
+	echo TESTS FAILED.
+fi
 
 umount /proc
 EOF
 
-$SUDO_CMD chroot $chroot_path sh /test_in_chroot.sh
+chroot $chroot_path sh /test_in_chroot.sh
 
