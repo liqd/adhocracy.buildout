fced5261afd630af888dbb03fe1e1dbc63cb3288
Philipp Hagemeister <phihag@phihag.de>
2012-05-14 13:47:49 +0200

    Correct path in paster_interactive.sh

--- etc/paster_interactive.sh
+++ etc/paster_interactive.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cd "$(dirname $(readlink -f $0))/.."
+if ! cd "$(dirname $(readlink -f $0))/../../"; then
+	echo 'Cannot find adhocracy_buildout directory!'
+	exit 2
+fi
 . bin/activate
 exec bin/paster serve --reload etc/adhocracy.ini
