dcb461a078630f9554d0691360cbbb6d5ac8e97e
Philipp Hagemeister <phihag@phihag.de>
2012-06-01 13:43:45 +0200

    Listen to 0.0.0.0 in paster_interactive.sh

--- etc/paster_interactive.sh
+++ etc/paster_interactive.sh
@@ -4,4 +4,10 @@ set -e
 
 cd "$(dirname $(readlink -f $0))/../../"
 . bin/activate
-exec bin/paster serve --reload etc/adhocracy.ini
+
+cp etc/adhocracy.ini etc/adhocracy-interactive.ini
+
+# Comment out the following line to restrict access to local only
+sed 's#host = .*#host = 0.0.0.0#' -i etc/adhocracy-interactive.ini
+
+exec bin/paster serve --reload etc/adhocracy-interactive.ini
